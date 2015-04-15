b5ad00c7b4c01372290b6542873a5f39302d9dcb
Luca Gentilini <luca.gentilini@student.unife.it>
2015-04-15 17:52:14 +0200

    init
    init

--- Attacco/src/main.py
+++ Attacco/src/main.py
@@ -3,7 +3,7 @@
 # and open the template in the editor.
 
 __author__ = "Luca"
-__date__ = "$15-apr-2015 17.44.24$"
+__date__ = "$15-apr-2015 17.51.59$"
 
 if __name__ == "__main__":
     print "Hello World"
